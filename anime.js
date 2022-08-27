1ff3cbd378f777a1171a5021c0f39052c0fed611
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-08-27 13:05:37 +0700

    add random quote function

--- anime.js
+++ anime.js
@@ -1,8 +1,11 @@
-fetch('https://github.com/rocktimsaikia/anime-chan/blob/main/images/animechan_logo.png?raw=true')
-			.then(response => response.json())
-			.then((data) => {
-				document.getElementById('quote').innerHTML = data.quote
-				document.getElementById('character').innerHTML = data.character
-				console.log(data);
-			}
-		)
+const randomQuote = () => {
+	fetch('https://animechan.vercel.app/api/random')
+	.then(response => response.json())
+	.then((data) => {
+		document.getElementById('quote').innerHTML = data.quote
+		document.getElementById('character').innerHTML = data.character
+		console.log(data);
+	})
+}
+
+randomQuote()
